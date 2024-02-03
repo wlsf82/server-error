e4e477c6f9b9e76a4ead57b519e1de3889fc0127
Walmyr <wlsf82@gmail.com>
2024-02-03 01:37:47 +0100

    Create cypress project

---
+++ README.md
@@ -0,0 +1,3 @@
+# server-error
+
+Sample project to demonstrate how to simulate a server error using the [`cy.intercept()`](https://on.cypress.io/intercept) command and [Cypress Playground](https://cypress-playground.s3.eu-central-1.amazonaws.com/index.html) as the application under test.
---
+++ cypress/e2e/serverError.cy.js
@@ -0,0 +1,20 @@
+describe('Server error', () => {
+  beforeEach(() => {
+    cy.visit('/index.html')
+  })
+
+  it('shows an error message when the server fails', () => {
+    cy.intercept(
+      'GET',
+      'https://jsonplaceholder.typicode.com/todos/1',
+      { statusCode: 500 }
+    ).as('serverError')
+    cy.contains('button', 'Get TODO').click()
+    cy.wait('@serverError')
+
+    cy.contains(
+      '.error span',
+      'Oops, something went wrong. Refresh the page and try again.'
+    ).should('be.visible')
+  })
+})
